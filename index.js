a11b8482d9253c67feb20d583c30c8c3b21bbb00
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-13 02:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
